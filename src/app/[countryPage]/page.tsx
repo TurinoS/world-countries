bee4e7c1b44f4258689c2941eb810be42cf7a3c1
Paulo Turino <paulo.turino01@gmail.com>
2023-06-19 11:08:22 -0300

    chore: estilizando a country page
    recebendo os dados e estilizando responsivamente

--- src/app/[countryPage]/page.tsx
+++ src/app/[countryPage]/page.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import styles from '../../styles/CountryPage.module.css';
 
 const getData = async (name: string) => {   
@@ -15,7 +16,18 @@ export default async function CountryPage() {
 
   const countryData = await getData('brazil')
 
-  console.log(countryData)
+  const countryInfo = [
+    countryData[0].name.official, 
+    countryData[0].population, 
+    countryData[0].region,
+    countryData[0].subregion,
+    countryData[0].capital,
+    countryData[0].demonyms.eng.m,
+    countryData[0].borders,
+    countryData[0].name.common,
+  ]
+
+  console.log(countryData[0])
   
   return (
     <main className={styles.main}>
@@ -25,15 +37,24 @@ export default async function CountryPage() {
         <div className={styles.info}>
           <h2>{countryData[0].name.common}</h2>
           <div>
-            <p><span>Native name:</span></p>
-            <p><span>Population:</span></p>
-            <p><span>Region:</span></p>
-            <p><span>Sub region:</span></p>
-            <p><span>Capital:</span></p>
-            <p><span>Currencies:</span></p>
-            <p><span>Languages:</span></p>
+            <p><span>Official name:</span> {countryInfo[0]}</p>
+            <p><span>Population:</span> {countryInfo[1]}</p>
+            <p><span>Region:</span> {countryInfo[2]}</p>
+            <p><span>Sub region:</span> {countryInfo[3]}</p>
+            <p><span>Capital:</span> {countryInfo[4]}</p>
+            <p><span>Demonym:</span> {countryInfo[5]}</p>
           </div>
         </div>
+        <div className={styles.borders}>
+          <h4>Border countries:</h4>
+          <ul>
+            {countryInfo[6] ? countryInfo[6].map((item: string ) => (
+              <li key={countryInfo[1]}>
+                <Link href={`/${countryInfo[7]}`}>{item}</Link>
+              </li>
+            )) : <p>{countryInfo[0]} is an island</p>}
+          </ul>
+          </div>
       </div>
     </main>
   )
